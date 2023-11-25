d8245493fc39e47657c390abb560b3414d5681bb
arpitaasth0 <arpitaasthana44332222@gmail.com>
2023-11-25 14:18:42 +0530

    second commit

--- routes/User.js
+++ routes/User.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const{login,signup} = require("../controllers/auth");
+const{login,signup,sendpasswordlink,forgotpassword,changepassword} = require("../controllers/auth");
 
 router.post("/login",login);
 router.post("/signup",signup);
+router.post("/sendpasswordlink",sendpasswordlink);
+router.get("/forgotpassword/:id/:token",forgotpassword);
+router.post("/:id/:token", changepassword);
 
 module.exports = router;
