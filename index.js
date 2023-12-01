b936d70291712baf02abe0694c75342a3ec2e4eb
arpitaasth0 <arpitaasthana44332222@gmail.com>
2023-12-01 23:27:21 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 require('dotenv').config();
 const PORT = process.env.PORT|| 4000;
 
 app.use(express.json());
+
+app.use(cors());
 
 require("./config/database").connect();
 
